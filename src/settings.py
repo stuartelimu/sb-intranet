325f9ca6315558f45a80d551f354fd26f4a79725
stuartelimu <stuartelimu@gmail.com>
2020-07-23 15:33:23 +0300

    add notifications

--- src/settings.py
+++ src/settings.py
@@ -14,6 +14,18 @@ import os
 from decouple import config
 import django_heroku
 
+# messages settings
+
+from django.contrib.messages import constants as messages
+
+MESSAGE_TAGS = {
+    messages.DEBUG: 'alert-info',
+    messages.INFO: 'alert-info',
+    messages.SUCCESS: 'alert-success',
+    messages.WARNING: 'alert-warning',
+    messages.ERROR: 'alert-danger',
+}
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
